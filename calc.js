9ff06a960473295687cb1aca0bb4fcdab117fd49
rinnegard <victor.rinnegard@hotmail.com>
2024-05-21 14:21:57 +0200

    Added functions and while loop for getting user input

--- calc.js
+++ calc.js
@@ -1,4 +1,36 @@
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')({
+    sigint: true
+});
 
-let number = prompt("Number?");
-console.log(number);
+
+function getNum(num) {
+    num = Number(prompt("Number?"));
+    return num;
+}
+
+function getOp(op) {
+    op = prompt("Operator?");
+    return op;
+}
+
+let numbers = [];
+let operators = [];
+
+while (operators[operators.length - 1] != "=") {
+    numbers.push(getNum());
+    operators.push(getOp());
+
+}
+
+console.log(numbers);
+console.log(operators);
+
+
+switch (operators) {
+    case "+":
+        console.log(number + number);
+        break;
+    default:
+        console.log("wops");
+        break;
+}
