209bc18ddde9d1896875d6552a9d4a2267247163
rinnegard <victor.rinnegard@hotmail.com>
2024-05-22 14:47:22 +0200

    Added functions for updating getting and clearing input field

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 console.log("Welcome to Calculator 1.0");
 
-let digits = document.querySelectorAll(".digit");
+function updateInput(value) {
+    input.value = input.value + value;
+}
 
-digits.forEach(button => {
+function getInput() {
+    let val = input.value;
+    clearInput();
+    return val;
+}
+
+function clearInput(value) {
+    input.value = "";
+}
+
+
+
+let digitButtons = document.querySelectorAll(".digit");
+
+digitButtons.forEach(button => {
     button.addEventListener("click", () => {
         updateInput(button.textContent);
     })
 });
 
-function updateInput(value) {
-    let input = document.querySelector("#num");
-    console.log(input);
-    input.value += value;
-}
+let clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener("click", () => {
+    clearInput();
+})
+
+let input = document.querySelector("#num");
+
+
+let operatorButtons = document.querySelectorAll(".operators button");
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(getInput());
+    })
+});
