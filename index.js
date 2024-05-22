cc64d55983a8ff02e525a18edf03490241d9fac5
rinnegard <victor.rinnegard@hotmail.com>
2024-05-22 14:12:03 +0200

    Added some html classes for javascript to select and added event for clicking numbers

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Welcome to Calculator 1.0");
+
+let digits = document.querySelectorAll(".digit");
+
+digits.forEach(button => {
+    button.addEventListener("click", () => {
+        updateInput(button.textContent);
+    })
+});
+
+function updateInput(value) {
+    let input = document.querySelector("#num");
+    console.log(input);
+    input.value += value;
+}
